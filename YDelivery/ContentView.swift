c56c7fa2931dbd4820b61b7aeb252c80233d5099
laconicman <laconicman@icloud.com>
2026-08-28 01:05:28 +0300

    Initial Commit

---
+++ YDelivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YDelivery
+//
+//  Created by Paul Buktab on 8/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YDelivery/YDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  YDeliveryApp.swift
+//  YDelivery
+//
+//  Created by Paul Buktab on 8/28/26.
+//
+
+import SwiftUI
+
+@main
+struct YDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
